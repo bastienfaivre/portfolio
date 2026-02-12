0ff50856f80ee4d1e75d81a04b6d0abdce8ca30d
Bastien Faivre <faivre.bast@gmail.com>
2026-02-12 19:34:06 +0100

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git stash
 git pull
 docker build -t portfolio:latest -f Dockerfile .
 docker stop portfolio || true
-docker run -d --restart unless-stopped --rm -p 3000:3000 --name portfolio portfolio:latest
+docker run -d --restart unless-stopped -p 3000:3000 --name portfolio portfolio:latest
 docker system prune -af
 
 trap - ERR
