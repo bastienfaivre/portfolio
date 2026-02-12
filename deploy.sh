254b784262ca0e4f8eef8eee66661d664a40e64e
Bastien Faivre <faivre.bast@gmail.com>
2026-02-12 19:39:43 +0100

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ git stash
 git pull
 docker build -t portfolio:latest -f Dockerfile .
 docker stop portfolio || true
+docker rm portfolio || true
 docker run -d --restart unless-stopped -p 3000:3000 --name portfolio portfolio:latest
 docker system prune -af
 
