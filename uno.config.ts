089f23a5790f879c24680ab461d20675177ca45b
Bastien Faivre <faivre.bast@gmail.com>
2026-03-13 15:10:37 +0100

    fix: updated packages

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, presetUno } from "unocss";
 
 export default defineConfig({
   content: {
-    filesystem: ["**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
+    filesystem: ["src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
   },
   theme: {
     breakpoints: {
